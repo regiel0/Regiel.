08300da084d5c1f44ed24e5a07b5f7416f58e972
regiel0 <163352495+regiel0@users.noreply.github.com>
2024-10-08 21:28:07 -0300

    Add files via upload

---
+++ REGIEL/perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris.'
+)
+criaCartao(
+    'Programação',
+        'O que é uma função?',
+        'É um bloco de código que executa alguma tarefa.'
+)
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês se diz Hi (rai).'
+)
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.'
+)
